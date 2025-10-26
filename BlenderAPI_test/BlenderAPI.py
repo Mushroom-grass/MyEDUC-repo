2ac80ccdca479deeb780bcb5031dfe6abde6406e
Yuanqi Hu <167298936+Mushroom-grass@users.noreply.github.com>
2025-10-26 17:37:31 -0400

    read all meshes and their materials

--- BlenderAPI_test/BlenderAPI.py
+++ BlenderAPI_test/BlenderAPI.py
@@ -1,6 +1,21 @@
 import bpy
 
-scene = bpy.data.scenes
+print("=======================")
+print("====== mesh and its materials ======")
 for obj in bpy.data.objects:
     if obj.type == 'MESH':
-        print(obj.name)
+#        print(obj.name + ": " + str(obj.data.materials))
+        for slot in obj.material_slots:
+            if slot.material != None:
+                print(obj.name + ": " + slot.material.name)
+
+        
+    
+print("===== Only Meshes =====")    
+for mesh in bpy.data.meshes:
+    if mesh.materials != None:
+        print(mesh.name + ": " + str(mesh.materials))
+    
+print("===== Only Materials =====")
+for mat in bpy.data.materials:
+    print(mat.name)
