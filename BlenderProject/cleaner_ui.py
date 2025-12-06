13f8c1f7828c3751bec7fcb34fa22633c593a650
Yuanqi Hu <167298936+Mushroom-grass@users.noreply.github.com>
2025-12-06 00:21:58 -0500

    1206 update

--- BlenderProject/cleaner_ui.py
+++ BlenderProject/cleaner_ui.py
@@ -1,29 +1,82 @@
-import bpy  # 导入Blender的Python API模块
+# 导入Blender的Python API模块
+import bpy, csv
+import pandas as pd
 
-class MyClassName(bpy.types.Operator):
-    bl_idname = "my_operator.my_class_name"
-    bl_label = "My Class Name"
-    bl_description = "Description that shows in blender tooltips"
+
+class MTtoCSV(bpy.types.Operator):
+    bl_idname = "my_operator.mt_to_csv"
+    bl_label = "Models' info to CSV"
+    bl_description = "Exporting models' information to csv file."
     bl_options = {"REGISTER"}
 
     @classmethod
     def poll(cls, context):
-        return True
+        # 只要场景不为空，就允许点击，即满足条件时返回True
+        return len(bpy.data.objects) > 0
 
+    # 执行的主程序
     def execute(self, context):
+        Object = []
+        Type = []
+        Material = []
+        MatSum = []
+        MatT = []
+
+        # 遍历场景中所有对象
+        for obj in bpy.data.objects: 
+            # 依次添加对象名字
+            Object.append(obj.name)
+            # 依次添加对象类别
+            Type.append(obj.type)
+            # 判断对象是否有材质，如果没有则直接依次写入none
+            if len(obj.material_slots) == 0:
+                Material.append('None')
+                MatSum.append('None')
+            else:
+                # 依次写入对象的材质数量
+                MatSum.append(len(obj.material_slots))
+                s_m = ''
+                # 读取一个对象的每个材质
+                for slot in obj.material_slots:
+                    s_m = slot.material.name + ',' + s_m
+                    MatT.append(slot.material.name)
+                # 将所有材质写入，并清理尾部逗号
+                Material.append(s_m.rstrip(','))
+
+        dict = {'object': Object, 'type': Type, 'material': Material, 'material number': MatSum}
+        df = pd.DataFrame(dict)
+        # save to csv
+        df.to_csv('/Users/yuanqihu/Library/CloudStorage/OneDrive-PennO365/2025Fall/EDUC5913/MyEDUC-repo/BlenderProject/secene_content.csv')
         
         return {"FINISHED"}
 
 
-    bl_idname = "object_data_csv"
-    bl_label = "export objects' info to csv"
-    def execute(self, context):
-        obj = context.object
-        self.report({'INFO'}, f"缩放值已报告: ({scale_x:.2f}, {scale_y:.2f}, {scale_z:.2f})")
-        return {'FINISHED'} # 操作成功完成
+class MTclean(bpy.types.Operator):
+    bl_idname = "my_operator.mt_cleaner"
+    bl_label = "Material and Texture cleaner"
+    bl_description = "A tool to help cleaning redundant materials or textures."
+    bl_options = {"REGISTER"}
 
-class MTCleaner(bpy.types.Panel):
-    #创建一个面板，在 3D 视图的侧边栏 (N面板) 中显示
+    @classmethod
+    def poll(cls, context):
+        return len(bpy.data.materials) > 0
+
+    def execute(self, context):
+        deleted_count = 0
+        for mat in bpy.data.materials:
+            # 判断材质的用户数量（即被对象使用的次数），如果 mat.users == 0，则该材质未被任何对象使用
+            if mat.users == 0:
+                # 删除该材质，do_unlink=True 会尝试解除所有链接。
+                bpy.data.materials.remove(mat, do_unlink=True)
+                deleted_count += 1
+
+        # 在 Blender 界面左下角显示结果
+        self.report({'INFO'}, f"清理完成。共删除 {deleted_count} 个未使用的材质。")
+        return {"FINISHED"}
+
+
+#创建一个面板，在 3D 视图的侧边栏 (N面板) 中显示
+class MTCleaner_ui(bpy.types.Panel):
     # 面板的标签
     bl_label = "Material and Texture Cleaner"
     # 面板的唯一ID
@@ -41,33 +94,28 @@ class MTCleaner(bpy.types.Panel):
         # 获取当前在 3D 视图中选中的活动物体 (Active Object)
         obj = context.object
 
+        layout.label(text = "------- 数据导出 -------")
         row = layout.row()
-        row.operator(OToCsv.bl_idname) # 调用 "object.report_scale"
+        row.operator(MTtoCSV.bl_idname) # 调用上面的执行函数
 
-        # 添加一个判断：只有选中了物体才显示功能，否则提示用户
-        # if obj:
-        #     row = layout.row()
-        #     row.label(text="Hello world!", icon='WORLD_DATA')
+        layout.separator()
 
-        #     row = layout.row()
-        #     row.label(text="Active object is: " + obj.name)
+        layout.label(text = "------- 场景清理 -------")
+        row = layout.row()
+        row.operator(MTclean.bl_idname) # 调用上面的执行函数
 
-        #     row = layout.row()
-        #     row.prop(obj, "name")
-
-        #     row = layout.row()
-        #     row.operator("mesh.primitive_cube_add")
-        # else:
-        #     # 如果没有选中物体，显示提示信息
-        #     layout.label(text="请先选中一个物体", icon="INFO")
 
 # 注册类和面板的函数
 def register():
-    bpy.utils.register_class(MTCleaner)
+    bpy.utils.register_class(MTtoCSV)
+    bpy.utils.register_class(MTclean)
+    bpy.utils.register_class(MTCleaner_ui)
 
 # 注销类和面板的函数
 def unregister():
-    bpy.utils.unregister_class(MTCleaner)
+    bpy.utils.unregister_class(MTtoCSV)
+    bpy.utils.unregister_class(MTclean)
+    bpy.utils.unregister_class(MTCleaner_ui)
 
 # 这部分代码确保如果脚本直接执行，面板会被注册
 if __name__ == "__main__":
